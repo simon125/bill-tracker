0ac69d574b637a4e441752e68e18d429d43fb6f5
Szymon Oleszek <szymqwerty@gmail.com>
2019-03-20 00:41:43 +0100

    created mobile version

--- src/components/Bills/index.js
+++ src/components/Bills/index.js
@@ -10,10 +10,18 @@ import FilterOptionsPanel from '../FilterOptionsPanel'
 import './style.css'
 import 'rc-slider/assets/index.css';
 
+const arrowStyle = {
+    border: "2px solid white", width: '25px', height: '25px', borderRadius: '50%', transition: 'all 0.3s'
+
+}
+
 class Bills extends Component {
     state = {
         bills: this.props.bills,
-        billsToDisplay: this.props.bills
+        billsToDisplay: this.props.bills,
+        maxHeight: '0px',
+        rotateDeg: '180'
+
     }
     setSortedBills = (sortedBills) => {
         this.setState((state) => {
@@ -28,9 +36,21 @@ class Bills extends Component {
             billsToDisplay: filtredBills
         })
     }
+    handleOnArrow = () => {
+
+        const maxHeight = this.state.maxHeight === "630px" ? '0px' : "630px";
+        const rotateDeg = this.state.rotateDeg === '0' ? '180' : '0';
+
+        this.setState({
+            maxHeight,
+            rotateDeg
+        })
+    }
     render() {
         const { bills, billsToDisplay } = this.state;
         const { maxPrice, shopNames } = this.props;
+        const isMobileDevice = window.innerWidth <= 550;
+
         return (
             <div className="container-fluid">
                 {
@@ -38,24 +58,28 @@ class Bills extends Component {
                         <h1 className="display-4 mt-5">You have no bills to render</h1>
                         :
                         <div className="row">
-                            <div className="col-4 col-md-3 col-lg-2">
-                                <div className="bg-secondary px-3 option-panel__container">
-                                    <h3 className="mt-3">
-                                        <i className="fas fa-sort ml-1"></i>
-                                        Sort by
-                                    </h3>
-                                    <SortOptionsPanel
-                                        bills={billsToDisplay}
-                                        setSortedBills={this.setSortedBills}
-                                    />
-                                    <FilterOptionsPanel
-                                        bills={bills}
-                                        shopNames={shopNames}
-                                        maxPrice={maxPrice}
-                                        setFilteredBills={this.setFilteredBills} />
+                            <div className={isMobileDevice ? "card card-body " : "col-4 col-md-3 col-lg-2"}>
+                                <div className={isMobileDevice ? "" : "bg-secondary px-3 option-panel__container"}>
+                                    <div className={isMobileDevice ? "d-flex justify-content-end" : "d-none"}>
+                                        <span className="mr-3">Choose filter/sort option</span>
+                                        <span
+                                            onClick={this.handleOnArrow}
+                                            style={{ ...arrowStyle, transform: `rotate(${this.state.rotateDeg}deg` }}><i className="fas fa-angle-up fa-lg"></i></span>
+                                    </div>
+                                    <div style={isMobileDevice ? { overflow: 'hidden', transition: 'all 0.3s', maxHeight: this.state.maxHeight } : {}}>
+                                        <SortOptionsPanel
+                                            bills={billsToDisplay}
+                                            setSortedBills={this.setSortedBills}
+                                        />
+                                        <FilterOptionsPanel
+                                            bills={bills}
+                                            shopNames={shopNames}
+                                            maxPrice={maxPrice}
+                                            setFilteredBills={this.setFilteredBills} />
+                                    </div>
                                 </div>
                             </div>
-                            <div className="col-8 col-md-9 col-lg-10 pr-4 options-panel__conainer--col-2">
+                            <div className={isMobileDevice ? "col-12 options-panel__conainer--col-2" : "col-8 col-md-9 col-lg-10 pr-4 options-panel__conainer--col-2"}>
 
                                 <div className="row">
                                     {billsToDisplay ? billsToDisplay.map(bill => <SingleBill key={Math.random()} bill={bill} />) : null}
